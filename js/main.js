3bb64a2d48c72ed9fe6588741782e61e1366b6bc
Shua Wolff <shtawo11@gmail.com>
2018-06-05 17:55:38 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen" , "queen" , "king" , "king"];
+
+var cardOne = cards[0];
+var cardsInPlay = [];
+
+cardsInPlay.push(cardOne)
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
